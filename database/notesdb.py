27e4b8b5cf8b4777971becd5da39c5964a09ce21
スタークギャング <StarkGangZ@gmail.com>
2021-04-02 09:30:01 +0530

    Update notesdb.py

--- database/notesdb.py
+++ database/notesdb.py
@@ -12,26 +12,26 @@ notes = db_x["NOTES"]
 
 
 async def add_note(keyword, chat_id, message_id):
-    stark = notes.find_one({"keyword": keyword})
+    stark = await notes.find_one({"keyword": keyword})
     if stark:
-        notes.update_one(
+        await notes.update_one(
             {"keyword": keyword},
             {"$set": {"chat_id": chat_id, "msg_id": message_id}},
         )
     else:
-        notes.insert_one({"keyword": keyword, "chat_id": chat_id, "msg_id": message_id})
+        await notes.insert_one({"keyword": keyword, "chat_id": chat_id, "msg_id": message_id})
 
 
 async def del_note(keyword, chat_id):
-    notes.delete_one({"keyword": keyword, "chat_id": chat_id})
+    await notes.delete_one({"keyword": keyword, "chat_id": chat_id})
 
 
 async def del_notes(chat_id):
-    notes.delete_many({"chat_id": chat_id})
+    await notes.delete_many({"chat_id": chat_id})
 
 
 async def note_info(keyword, chat_id):
-    r = notes.find_one({"keyword": keyword, "chat_id": chat_id})
+    r = await notes.find_one({"keyword": keyword, "chat_id": chat_id})
     if r:
         return r
     else:
@@ -39,7 +39,7 @@ async def note_info(keyword, chat_id):
 
 
 async def all_note(chat_id):
-    r = list(notes.find({"chat_id": chat_id}))
+    r = [u async for u in notes.find({"chat_id": chat_id})]
     if r:
         return r
     else:
