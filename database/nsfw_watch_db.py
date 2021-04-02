f9da672030fe7d95a14c86eb0a3772565db2919c
スタークギャング <StarkGangZ@gmail.com>
2021-04-02 09:30:55 +0530

    Update nsfw_watch_db.py

--- database/nsfw_watch_db.py
+++ database/nsfw_watch_db.py
@@ -12,20 +12,20 @@ nsfw = db_x["NSFW_WATCH"]
 
 
 async def add_chat(chat_id):
-    nsfw.insert_one({"chat_id": chat_id})
+    await nsfw.insert_one({"chat_id": chat_id})
 
 
 async def rm_chat(chat_id):
-    nsfw.delete_one({"chat_id": chat_id})
+    await nsfw.delete_one({"chat_id": chat_id})
 
 
 async def get_all_nsfw_chats():
-    lol = list(nsfw.find({}))
+    lol = [kek async for kek in nsfw.find({})]
     return lol
 
 
 async def is_chat_in_db(chat_id):
-    k = nsfw.find_one({"chat_id": chat_id})
+    k = await nsfw.find_one({"chat_id": chat_id})
     if k:
         return True
     else:
