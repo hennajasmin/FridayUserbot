d7dd793e8b84cc8c34944aad7c1259a07cf42eb8
スタークギャング <StarkGangZ@gmail.com>
2021-04-02 09:24:40 +0530

    Update chatbot_db.py

--- database/chatbot_db.py
+++ database/chatbot_db.py
@@ -13,20 +13,20 @@ cbb = db_x["blacklisted_users"]
 
 
 async def add_chatbotuser(user_id):
-    chatbot_user_db.insert_one({"user_id": user_id})
+    await chatbot_user_db.insert_one({"user_id": user_id})
 
 
 async def rmchatbotuser(user_id):
-    chatbot_user_db.delete_one({"user_id": user_id})
+    await chatbot_user_db.delete_one({"user_id": user_id})
 
 
 async def get_all_chatbotusers():
-    lol = list(chatbot_user_db.find())
+    lol = [ko async for ko in chatbot_user_db.find()]
     return lol
 
 
 async def is_chatbotuser_in_db(user_id):
-    k = chatbot_user_db.find_one({"user_id": user_id})
+    k = await chatbot_user_db.find_one({"user_id": user_id})
     if k:
         return True
     else:
@@ -34,15 +34,15 @@ async def is_chatbotuser_in_db(user_id):
 
 
 async def add_blacklisted_user(user_id):
-    cbb.insert_one({"user_id": user_id})
+    await cbb.insert_one({"user_id": user_id})
 
 
 async def rm_blacklisted_user(user_id):
-    cbb.delete_one({"user_id": user_id})
+    await cbb.delete_one({"user_id": user_id})
 
 
 async def is_user_blacklisted(user_id):
-    b = cbb.find_one({"user_id": user_id})
+    b = await cbb.find_one({"user_id": user_id})
     if b:
         return True
     else:
